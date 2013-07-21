213148ef6178c22886b1849a106a4b813c38e7f4
Patrick Kann <pkkann@gmail.com>
2013-07-21 21:49:25 +0200

    new

---
+++ Cressen/src/cressen/Cressen.java
@@ -0,0 +1,17 @@
+
+package cressen;
+
+/**
+ *
+ * @author patrick
+ */
+public class Cressen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
