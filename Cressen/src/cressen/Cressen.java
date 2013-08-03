f203b588ce7c6248275ac387f21be0287bce70e5
Tobias <webdota@hotmail.com>
2013-08-03 21:09:12 +0200

    hey

--- Cressen/src/cressen/Cressen.java
+++ Cressen/src/cressen/Cressen.java
@@ -1,32 +1,17 @@
 
 package cressen;
 
-import cressen.console.LoginConsole;
-import cressen.console.TheConsole;
-import cressen.user.User;
-import cressen.user.UserManager;
-
 /**
  *
- * @author patrick
+ * @author Patrick
  */
 public class Cressen {
-    
-    private UserManager userManager;
-    private TheConsole theConsole;
-    private LoginConsole loginConsole;
-    
-    public Cressen() {
-        userManager = new UserManager();
-        theConsole = new TheConsole();
-        loginConsole = new LoginConsole(userManager);
-    }
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        Cressen cressen = new Cressen();
-        cressen.userManager.addUser(new User("pkkann", "rollercoaster", "pkkann@gmail.com", "Patrick Kann"));
-        cressen.loginConsole.start();
-        
+        // TODO code application logic here
     }
 
 }
